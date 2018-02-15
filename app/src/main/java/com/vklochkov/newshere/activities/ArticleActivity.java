01cb711c4ed9da17b7dff9d98e1cbcd2307d1c7b
Vladislav Klochkov <kvladislavo2013@gmail.com>
2018-02-15 23:14:52 +0300

    Added progress bar to article activity.

--- app/src/main/java/com/vklochkov/newshere/activities/ArticleActivity.java
+++ app/src/main/java/com/vklochkov/newshere/activities/ArticleActivity.java
@@ -2,27 +2,47 @@ package com.vklochkov.newshere.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import com.vklochkov.newshere.R;
 
 public class ArticleActivity extends AppCompatActivity {
+    private ProgressBar progressBar;
+
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article);
+
+        progressBar = findViewById(R.id.article_progress_bar);
 
         setTitle(getIntent().getStringExtra("title") + "/reading");
         initializeWebView(getIntent().getStringExtra("url"));
     }
 
     private void initializeWebView (String url) {
-        WebView webview = findViewById(R.id.webview);
-        WebSettings webSettings = webview.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webview.loadUrl(url);
+        WebView webView = findViewById(R.id.web_view);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+                progressBar.setProgress(progress);
+
+                if (progress == 100) {
+                    progressBar.setVisibility(View.GONE);
+
+                } else {
+                    progressBar.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        webView.loadUrl(url);
     }
 
     private void setTitle (String title) {
